60449e8d9543fb99d5a4df354af7c985e32bc9c0
Gabriel Toll Stålbom <gabtost@gmail.com>
2023-08-16 11:40:02 +0200

    fix export

--- sequential-id.ts
+++ sequential-id.ts
@@ -2,10 +2,12 @@ let num = 0;
 const LEN = 5;
 const PAD = "0";
 
-export default function sequentialId() {
+function sequentialId() {
   let id = String(++num);
   while (id.length < LEN) {
     id = PAD + id;
   }
   return id;
 }
+
+export = sequentialId;
