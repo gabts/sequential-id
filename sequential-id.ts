a1a0dc4994d4582d166348d9397fd965d6c995ba
Gabriel Toll Stålbom <gabtost@gmail.com>
2023-08-16 11:53:17 +0200

    refactor to named export

--- sequential-id.ts
+++ sequential-id.ts
@@ -2,12 +2,10 @@ let num = 0;
 const LEN = 5;
 const PAD = "0";
 
-function sequentialId() {
+export function sequentialId() {
   let id = String(++num);
   while (id.length < LEN) {
     id = PAD + id;
   }
   return id;
 }
-
-export = sequentialId;
